47d02821584a18b8d59239bc12cbbdc3e994cc96
BrasilicioHenrique <166045710+BrasilicioHenrique@users.noreply.github.com>
2024-08-01 11:03:19 -0300

    p01.py

--- src/python/p01.py
+++ src/python/p01.py
@@ -1,23 +1,37 @@
+while True:
+    def mostrar_menu():
+        menu = """
+        ====================
+        SISTEMA ESCOLAR
+        ====================
+        1. Cadastrar novo estudante
+        2. Listar todos os estudantes
+        3. Atualizar dados de um estudante
+        4. Excluir um estudante
+        5. Sair
+        ====================
+        """
+        print(menu)
 
-def mostrar_menu():
-    menu = """
-    ====================
-       SISTEMA ESCOLAR
-    ====================
-    1. Cadastrar novo estudante
-    2. Listar todos os estudantes
-    3. Atualizar dados de um estudante
-    4. Excluir um estudante
-    5. Sair
-    ====================
-    """
-    print(menu)
+    mostrar_menu()
 
-def opcao_invalida():
-    print("Opção inválida, por favor selecione uma opção válida.")
+    opcao = [1, 2, 3, 4]
+    escolha_menu = int(input("Escolha uma opcao: "))
 
-def main():
-    pass
+    def opcao_invalida():
+        print("Opção inválida, por favor selecione uma opção válida.")
 
-if __name__ == "__main__":
-    main()
+    def main():
+        pass
+
+    if __name__ == "__main__":
+        main()
+
+    if escolha_menu in opcao:
+        continue
+    elif escolha_menu > 5:
+        opcao_invalida()
+        continue
+    elif escolha_menu == 5:
+        print("Finalizando...")
+        exit()
